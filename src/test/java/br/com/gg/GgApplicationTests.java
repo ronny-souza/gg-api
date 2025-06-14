54f24d1e60123ce1451023b63b78fc6938e1fa8e
ronny-souza <ronnymarinho2@gmail.com>
2025-06-14 16:18:09 -0300

    🎉 init: inicializando o projeto

---
+++ src/test/java/br/com/gg/GgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
